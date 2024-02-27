30e839fcb2fbd7e251f4cac89cd2d8c1aacf8ee2
Nuria Robledo <nuriarobledo0@gmail.com>
2024-02-27 14:50:48 -0300

    agregue las rutas

---
+++ src/screens/Factura.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Factura = () => {
+  return (
+    <div><h1>Aca va a estar el liostado de Factura</h1></div>
+  )
+}
+
+export default Factura
---
+++ src/screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Esta es la pagina de home</h1>
+        <h2>aca las opciones del menu: Listado facturas</h2></div>
+  )
+}
+
+export default Home
