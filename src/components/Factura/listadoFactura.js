ad1a74c453215f019c587e8f76eb518ba3cc1a5d
Nuria Robledo <nuriarobledo0@gmail.com>
2024-03-01 02:53:12 -0300

    funcionando modal con datos bd

--- src/components/Factura/listadoFactura.js
+++ src/components/Factura/listadoFactura.js
@@ -1,40 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import axios from 'axios';
-import ModalFactura from '../modalFactura'
+import ModalFactura from '../modalFactura';
 import Cookies from 'universal-cookie';
 
-const cookies = new Cookies();
 
 const ListadoFactura = () => {
+    const cookies = new Cookies();
 
     const [facturas, setFacturas] = useState([]);
     const [mostrarModal, setMostrarModal] = useState(false);
+    const [facturaSeleccionada, setFacturaSeleccionada] = useState(null);
+    const [idFactura, setIdFactura] = useState(null);
+    const [dataFactura, setDataFactura] = useState([]);
 
-    const abrirModal = () => {
+
+    const abrirModal = async (numeroFactura) => {
         setMostrarModal(true);
+        setFacturaSeleccionada(numeroFactura);
+        await obtenerIdPorNumero(numeroFactura);
     };
 
     const cerrarModal = () => {
         setMostrarModal(false);
     };
-
-
-    useEffect(() => {
-        obtenerFacturasPorObraSocial();
-
-        // Suscripción a cambios en la cookie 'obraSocial'
-        const handleChange = () => {
-            obtenerFacturasPorObraSocial();
-        };
-
-        cookies.addChangeListener(handleChange);
-
-        // Retirar el listener cuando el componente se desmonta
-        return () => {
-            cookies.removeChangeListener(handleChange);
-        };
-    }, []); // <-- Importante: Dejar la dependencia vacía para que se ejecute solo una vez al montar el componente
 
     const obtenerFacturasPorObraSocial = async () => {
         try {
@@ -45,6 +34,46 @@ const ListadoFactura = () => {
             console.error('Error al obtener las facturas:', error);
         }
     };
+
+
+    const obtenerIdPorNumero = async (facturaSeleccionada) => {
+        try {
+            const response = await axios.get(`https://localhost:7147/Factura/IdxNumero/${facturaSeleccionada}`);
+
+            setIdFactura(response.data.id);
+            console.log('El id de la factura seleccionada es:', idFactura);
+
+            // Aquí llamamos a getFacturaPorId después de establecer el id de la factura
+            await getFacturaPorId(response.data.id);
+
+        } catch (error) {
+            console.error('Error al obtener el id: ', error)
+        }
+    };
+    const getFacturaPorId = async (idFactura) => {
+        try {
+            const response = await axios.get(`https://localhost:7147/Factura/${idFactura}`);
+            setDataFactura(response.data);
+            console.log('La data de la factura seleccionada es:', response.data);
+        }
+        catch (error) {
+            console.error('Error al obtener la factura : ', error)
+        }
+    }
+
+    useEffect(() => {
+        obtenerFacturasPorObraSocial();
+        // Suscripción a cambios en la cookie 'obraSocial'
+        const handleChange = () => {
+            obtenerFacturasPorObraSocial();
+        };
+        cookies.addChangeListener(handleChange);
+        // Retirar el listener cuando el componente se desmonta
+        return () => {
+            cookies.removeChangeListener(handleChange);
+        };
+    }, []); // <-- Importante: Dejar la dependencia vacía para que se ejecute solo una vez al montar el componente
+
 
     return (
         <div>
@@ -66,9 +95,12 @@ const ListadoFactura = () => {
                             <td>{factura.estadoPago}</td>
                             <td>{factura.importeTotal}</td>
                             <td>{factura.fechaVencimiento}</td>
-                                    
                             <td colSpan={2}>
-                                <button className='btn btn-success' onClick={abrirModal} disabled={mostrarModal}>
+                                <button
+                                    className='btn btn-success'
+                                    onClick={() => abrirModal(factura.numero)}
+                                    disabled={mostrarModal}
+                                >
                                     Cobrar
                                 </button>
                             </td>
@@ -77,7 +109,7 @@ const ListadoFactura = () => {
                 </tbody>
             </Table>
             {/* Renderiza el modal solo si mostrarModal es verdadero */}
-            {mostrarModal && <ModalFactura onClose={cerrarModal} />}
+            {mostrarModal && <ModalFactura onClose={cerrarModal} factura={dataFactura} />}
         </div>
     )
 }
--- src/components/modalFactura.js
+++ src/components/modalFactura.js
@@ -1,53 +1,58 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal, Button, Col, Row, Container } from 'react-bootstrap';
+import axios from 'axios';
+
+function ModalFactura({ onClose, factura }) {
+  const [showModal, setShowModal] = useState(true);
+  
+
+  const handleClose = () => {
+    setShowModal(false);
+    onClose(); // Llama a la función onClose para notificar al componente padre que el modal se cerró
+  };
 
 
-function ModalFactura({ onClose }) {
-    const [showModal, setShowModal] = useState(true);
-
-    const handleClose = () => {
-        setShowModal(false);
-        onClose(); // Llama a la función onClose para notificar al componente padre que el modal se cerró
-    };
-
-    return (
-        <Modal show={showModal} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Alta cobranza</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-            <Container>
-          <Row className="border">
-            <Col xs={12} md={8}>
-              Fecha: xxxxxxxx
-            </Col>
-            <Col xs={6} md={4}>
-              numero
-            </Col>
-          </Row>
-
-          <Row>
-          <Col xs={12} md={8}>
-              Numero de factura: xxxxxxxx
-            </Col>
-          </Row>
-          <Row>
-          <Col xs={12} md={8}>
-              Monto a pagar: xxxxxxxx
-            </Col>
-          </Row>
-        </Container>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="primary" onClick={handleClose}>
-                    Registrar
-                </Button>
-                <Button variant="secondary" onClick={handleClose}>
-                    Cancelar
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+  return (
+    <Modal show={showModal} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Alta cobranza</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {factura ? (
+          <Container>
+            <Row className="border">
+              <Col xs={12} md={8}>
+                fecha:
+              </Col>
+              <Col xs={6} md={4}>
+                Numero:
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={8}>
+                Numero: {factura.numero}
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={8}>
+                Importe: {factura.importeTotal}
+              </Col>
+            </Row>
+          </Container>
+        ) : (
+          <p>No se ha proporcionado ninguna factura</p>
+        )}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={handleClose}>
+          Registrar
+        </Button>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancelar
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default ModalFactura;
